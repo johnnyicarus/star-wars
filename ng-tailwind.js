7bcf494a4f26a2a62fc7745f0df63b0d403724aa
Marin Aeschbach <marin.aeschbach@gmail.com>
2018-11-14 19:48:48 +0100

    build: add tailwind

---
+++ ng-tailwind.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Tailwind Paths
+  configJS: '/Users/Marin/Development/Angular/star-wars/tailwind.js',
+  sourceCSS: '/Users/Marin/Development/Angular/star-wars/src/tailwind.css',
+  outputCSS: '/Users/Marin/Development/Angular/star-wars/src/styles.css',
+  // PurgeCSS Settings
+  purge: false,
+  keyframes: false,
+  fontFace: false,
+  rejected: false,
+  whitelist: [],
+  whitelistPatterns: [],
+  whitelistPatternsChildren: []
+}
